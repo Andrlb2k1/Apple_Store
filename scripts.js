837d49763defa97e4bb600951b5ab4f01de4d394
unknown <andre.lb865854@gmail.com>
2025-12-08 11:25:12 -0300

    scripts and animations

--- scripts.js
+++ scripts.js
@@ -7,18 +7,39 @@ const list = document.querySelector('.list');
 
 let active = 0;
 const total = items.length;
-let time;
+let timer;
 
 function update(direction) {
     document.querySelector('.item.active').classList.remove('active');
     document.querySelector('.dot.active').classList.remove('active');
 
-    // if(direction > 0) {
+    if(direction > 0) {
+        active += 1;
 
-    // } else if(direction < 0) {
+        if(active === total) {
+            active = 0;
+        }
 
-    // }
+    } else if(direction < 0) {
+        active -= 1;
+
+        if(active < 0) {
+            active = total - 1;
+        }
+
+    }
+
+    items[active].classList.add('active');
+    dots[active].classList.add('active');
+
+    numberIndicator.textContent = String(active + 1).padStart(2, '0');
 }
+
+clearInterval(timer);
+timer = setInterval(() => {
+    update(1);
+}, 5000);
+// 5000ms = 5s
 
 prevButton.addEventListener("click", () => {
     update(-1)
